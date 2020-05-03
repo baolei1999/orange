9c8a359dcfe38ab09661751b8f3c88365912e8c9
linchunyan <710768347@qq.com>
2020-05-03 16:30:06 +0800

    添加首页

--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import index from "./index.module.css"
 
 class Index extends Component {
     render() {
         return (
             <div>
-                我是首页
+                <header className={index.linHeader}>
+                    <div className={index.headerLocation} >全国</div>
+                    <div>
+                        <input  type={"text"}  />
+                    </div>
+                    <div className={index.headerLocation}>日历</div>
+                </header>
+                <div className={index.banner}>
+                    <div className={index.bannerContent}></div>
+                </div>
             </div>
         );
     }
