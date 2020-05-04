171785a6daa8c8edc4c351465d12192c08b5421f
linchunyan <710768347@qq.com>
2020-05-05 01:50:05 +0800

    轮播图

--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -1,41 +1,64 @@
 import React, { Component } from 'react';
 import index from "./index.module.css"
 import axios from "axios"
+import Swiper from 'swiper'
+// import 'swiper/dist/css/swiper.min.css'
 class Index extends Component {
 
 
     constructor() {
         super();
         this.state = {
-            page: 1,
+            page: 0,
             navList: [],
-            recommendList:[]
+            recommendList: []
         }
     }
 
     async getNavList() {
-        const { data } = await axios.get("/cheng/home/index/getClassifyHome", {
-            params: {
-                page: this.state.page
-            }
-        })
+        const { data } = await axios.get("/cheng/home/index/getClassifyHome")
         // console.log(data.data.classify_list)
         this.setState({
             navList: data.data.classify_list
         })
-       
+
     }
-    async getRecommend(){
-        const {data} =await axios.get("/cheng/Show/Search/getShowList")
-        console.log(data.data.list)
-        this.setState({
-            recommendList:data.data.list
+    async getRecommend() {
+        this.state.page++
+        const { data } = await axios.get("/cheng/Show/Search/getShowList", {
+            params: {
+                page: this.state.page
+            }
         })
-    
+        // console.log(data.data.list)
+        this.setState({
+            recommendList: [
+                ...this.state.recommendList,
+                ...data.data.list
+
+            ]
+        })
     }
     async componentDidMount() {
         this.getNavList()
         this.getRecommend()
+
+
+        var mySwiper = new Swiper('.swiper-container', {
+            direction: 'horizontal',
+            autoplay: {
+                delay: 2000,
+                disableOnInteraction: false,
+                waitForTransition: false,
+              },
+              pagination: {
+                el: '.swiper-pagination',
+                dynamicBullets: true,
+              },
+        })
+
+
+
     }
 
     render() {
@@ -54,10 +77,40 @@ class Index extends Component {
                         <img src="../assets/img/2.png"></img>
                     </div>
                 </header>
-                <div className={index.banner}>
-                    <div className={index.bannerContent}></div>
+                    <div className='banner' >
+                    <div className='swiper-container' >
+                        <div className='swiper-wrapper ' style={{ width: "12400px"}}>
+                        
+
+                            <div className='swiper-slide index.slide ' style={{ float: "left" }}>
+                                <img src="https://image.juooo.com/group1/M00/03/94/rAoKmV59ZUyAfU1aAAESuLTnTAw739.png" />
+                            </div>
+                            <div className='swiper-slide' style={{ float: "left" }}>
+                                <img src="https://image.juooo.com/group1/M00/03/6D/rAoKmV4B3DeAAheBAAE8k0yWnNo649.png" />
+                            </div>
+                            <div className='swiper-slide' style={{ float: "left" }}>
+                                <img src="https://image.juooo.com/group1/M00/03/6D/rAoKmV4B3CmARboCAACCt7TZJ7k095.png" />
+                            </div>
+                            <div className='swiper-slide' style={{ float: "left" }}>
+                                <img src="https://image.juooo.com/group1/M00/03/6D/rAoKmV4B29-AdwTZAAB-i9-En8E765.png" />
+                            </div>
+                            <div className='swiper-slide' style={{ float: "left" }}>
+                                <img src="https://image.juooo.com/group1/M00/04/17/rAoKNV4B2zOADywIAACKVI3TINE298.png" />
+                            </div>
+                            <div className='swiper-slide' style={{ float: "left" }}>
+                                <img src="https://image.juooo.com/group1/M00/04/40/rAoKNV6W4PKACOg2AAEj0bMT7bw022.png" />
+                            </div>
+                            <div className='swiper-slide' style={{ float: "left" }}>
+                                <img src="https://image.juooo.com/group1/M00/04/3E/rAoKNV6ClgWAOtm_AAEhpdmQomE538.jpg" />
+                            </div>
+                       
+                        </div>
+                        
+                    </div>
+                   
                 </div>
-                <div className={index.list}>
+                  
+                <div className={index.list} style={{height:"550px"}}>
                     {
                         this.state.navList.map(v => (
                             <div className={index.listChildren} key={v.id}>
@@ -69,8 +122,6 @@ class Index extends Component {
                         ))
 
                     }
-
-
 
                 </div>
 
@@ -96,28 +147,28 @@ class Index extends Component {
                 <div className={index.Recommend}>
                     <h3>为你推荐</h3>
                     {
-                        this.state.recommendList.map(v=>(
+                        this.state.recommendList.map(v => (
                             <div key={v.schedular_id} className={index.recommendList}>
-                               <a >
-                                    <img style={{width:"340px" ,height:"462px"}}src={v.pic} />   
+                                <a >
+                                    <img style={{ width: "340px", height: "462px" }} src={v.pic} />
                                 </a>
                                 <div>
-                                    <img src={v.method_icon}/>
+                                    <img src={v.method_icon} />
                                     <span>
                                         {v.name}
                                     </span>
                                     <p>
-                                      {  v.start_show_time}
+                                        {v.start_show_time}
                                     </p>
                                     <p>
-                                      ￥{  v.min_price}起
+                                        ￥{v.min_price}起
                                     </p>
                                     <div>
                                         {
-                                            v.support_desc.map(v=>(
+                                            v.support_desc.map(v => (
                                                 <div key={v}>
                                                     <span>{v}</span>
-                                                    
+
 
                                                 </div>
                                             ))
@@ -127,7 +178,13 @@ class Index extends Component {
                             </div>
                         ))
                     }
+                    <input type={"button"} value={"加载更多"} onClick={this.getRecommend.bind(this)} />
                 </div>
+
+
+
+
+
             </div>
         );
     }
