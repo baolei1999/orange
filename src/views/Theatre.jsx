d3df66a4a8c9f1acee3143318aef404a16fe5de3
baolei <1138515583@qq.com>
2020-05-06 15:29:05 +0800

    剧院页面结束

--- src/views/Theatre.jsx
+++ src/views/Theatre.jsx
@@ -13,13 +13,20 @@ class theatre extends Component {
         }
     }
     render() {
+        const theatre_list=[]
+        this.state.theatre_list.find(v=>{
+            if(v.showList.length>0){
+                theatre_list.push(v)
+            }
+        })
+        console.log(theatre_list)
         return (
             <div className={thbao.big}>
                 <header>剧院</header>
                 <main>
                     <ul>
                         {
-                            this.state.theatre_list.map(v=>(
+                            theatre_list.map(v=>(
                                 <li key={v.id}>
                                     <div className={thbao.top}>
                                         <img className={thbao.one} height="100" width="100" src={v.pic} alt=""/>
@@ -28,7 +35,21 @@ class theatre extends Component {
                                         <img className={thbao.two} src={require("../assets/img/more.png")} alt=""/>
                                     </div>
                                     <div className={thbao.bottom}>
-                                        {/* <lunbo1></lunbo1> */}
+                                        <div className={thbao.boxx}>
+                                            {
+                                                v.showList.map(v1=>( 
+                                                    <div className={thbao.small} key={v1.id}>
+                                                        <div className={thbao.one}>
+                                                            <div className="show-data">{v1.show_time}</div>
+                                                            <span></span>
+                                                        </div>
+                                                        <div className={thbao.two}>
+                                                            <img src={v1.pic} width="214" height="292" alt=""/>
+                                                        </div>
+                                                    </div>
+                                                ))
+                                            }
+                                        </div>
                                     </div>
                                 </li>
                             ))
