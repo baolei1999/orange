7c637c711a3f0ddb3fb46fae71684416b155b435
baolei <1138515583@qq.com>
2020-05-06 21:10:22 +0800

    search部分页面

--- src/views/Search.jsx
+++ src/views/Search.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import sebao from "./search.module.css"
 
 export default class Search extends Component {
     render() {
         return (
-            <div>
-                ss 
+            <div className={sebao.big}>
+                <header>
+                    <div className={sebao.search}>
+                        <img src={require("../assets/img/search.png")} alt=""/>
+                        <input type="text" placeholder="搜索热门演出" />
+                    </div>
+                    <input type="button" className={sebao.btn} value="取消"/>
+                </header>
+                <main></main>
             </div>
         )
+    }
+    componentDidMount(){
+        console.log(this.props.location.state)
     }
 }
